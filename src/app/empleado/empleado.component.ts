3de744629904c79d5cbca22a8cbfabecf29e865a
unknown <richardgodplay@gmail.com>
2021-02-10 12:42:21 -0500

    Cambiado a estilos en linea

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
-  //template: "<p>Aqui iria un empleado</p>",
-  styleUrls: ['./empleado.component.css']
-  //styles: ["p{background-color: red;}"]
+  //templateUrl: './empleado.component.html',
+  template: "<p>Aqui iria un empleado</p>",
+  //styleUrls: ['./empleado.component.css']
+  styles: ["p{background-color: red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
