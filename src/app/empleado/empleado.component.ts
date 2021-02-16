d7f7c2fdfff1ef8eb638ef84b2618f7cae86b2ae
Sergio Sanchez <richardgodplay@gmail.com>
2021-02-16 14:10:09 -0500

    Interpolacion de Strings

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,12 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  //templateUrl: './empleado.component.html',
-  template: "<p>Aqui iria un empleado</p>",
-  //styleUrls: ['./empleado.component.css']
-  styles: ["p{background-color: red;}"]
+  templateUrl: './empleado.component.html',
+  //template: "<p>Aqui iria un empleado</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color: red;}"]
 })
 export class EmpleadoComponent implements OnInit {
+
+  nombre = "Sergio";
+
+  apellido = "Sanchez";
+
+  edad = 27;
+
+  empresa = "Sony Interactive Entertainment";
+
+  /*getEdad(){
+
+    return this.edad;
+
+  }*/
+
 
   constructor() { }
 
